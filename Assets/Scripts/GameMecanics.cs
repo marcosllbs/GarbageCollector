e1072b7974257628f96e402c57caa92e26b05900
Marcos Lucas <marcosllbs@hotmail.com>
2024-09-16 17:23:45 -0300

    Ramdomize objects and positions to fall objects system implementation

--- Assets/Scripts/GameMecanics.cs
+++ Assets/Scripts/GameMecanics.cs
@@ -7,13 +7,41 @@ public class GameMecanics : MonoBehaviour
     [SerializeField] List<GameObject> placesToInstantiateObjects;
     [SerializeField] List<GameObject> objectsToInstantiate;
 
-    public void RamdomizePlaceToInstantiate()
+    public float timeInterval = 5f;
+    public float countDown = 0f;
+
+    void Update()
+    {
+
+        countDown += Time.deltaTime;
+        if (countDown >= timeInterval)
+        {
+            Instantiate(objectsToInstantiate[RamdomizeObjectToInstantiate()], placesToInstantiateObjects[RamdomizePlaceToInstantiate()].transform);
+            countDown = 0f;
+        }
+
+    }
+
+    void Start()
     {
 
     }
 
-    public void RamdomizeObjectToInstantiate()
+    public int RamdomizePlaceToInstantiate()
     {
+        int number;
 
+        number = Random.Range(0, placesToInstantiateObjects.Count);
+
+        return number;
+    }
+
+    public int RamdomizeObjectToInstantiate()
+    {
+        int number;
+
+        number = Random.Range(0, objectsToInstantiate.Count);
+
+        return number;
     }
 }
